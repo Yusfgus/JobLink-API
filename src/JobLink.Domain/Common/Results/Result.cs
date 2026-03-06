59f7ddfa75fa04a3cf0fe0049d1e6318e43d8a97
UC <yousefgus111@gmail.com>
2026-03-06 16:11:22 +0200

    add Result Pattern

---
+++ src/JobLink.Domain/Common/Results/Result.cs
@@ -0,0 +1,50 @@
+namespace JobLink.Domain.Common.Results;
+
+public class Result
+{
+    public List<Error> Errors { get; } = default!;
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+
+    protected Result()
+    {
+        Errors = [];
+        IsSuccess = true;
+    }
+
+    protected Result(List<Error> errors)
+    {
+        Errors = errors ?? throw new ArgumentNullException(nameof(errors));
+        IsSuccess = false;
+    }
+
+    public static Result Success() => new();
+
+    public static implicit operator Result(Error error) => new([error]);
+    public static implicit operator Result(List<Error> errors) => new(errors);
+
+    public TResult Match<TResult>(Func<TResult> onSuccess, Func<List<Error>, TResult> onFailure)
+        => IsSuccess ? onSuccess() : onFailure(Errors);
+}
+
+public class Result<T> : Result
+{
+    public T? Value { get; }
+
+    private Result(List<Error> errors) : base(errors)
+    {
+        Value = default;
+    }
+
+    private Result(T value) : base()
+    {
+        Value = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public static implicit operator Result<T>(T value) => new(value);
+    public static implicit operator Result<T>(Error error) => new([error]);
+    public static implicit operator Result<T>(List<Error> errors) => new(errors);
+
+    public TResult Match<TResult>(Func<T, TResult> onSuccess, Func<List<Error>, TResult> onFailure)
+        => IsSuccess ? onSuccess(Value!) : onFailure(Errors);
+}
